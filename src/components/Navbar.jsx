eb1c4c937df4dabc6d6648277d503379d879d2fb
johnny <jhuang007007@gmail.com>
2023-06-21 13:49:25 -0400

    initial draft for hero and about sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+
         <Link 
           to='/'
           className='flex items-center gap-2'
@@ -25,6 +26,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className='w-20 h-20 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>Johnny Huang<span className='sm:block hidden'>Web Developer</span></p>
         </Link>
+
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
             <li
@@ -40,6 +42,8 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        {/* mobile */}
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img 
           src={toggle ? close : menu} 
